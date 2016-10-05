1b7e8bd4e0144e2006119966b84ab25e39b08bd3
Akos Murati <akos@murati.hu>
2016-10-06 00:32:18 +1300

    Initial MP3 Tracklist population

--- test/OszkConnector.Tests/OszkConnectorTests.cs
+++ test/OszkConnector.Tests/OszkConnectorTests.cs
@@ -2,6 +2,8 @@
 using OszkConnector.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Tests
@@ -9,11 +11,22 @@ namespace Tests
     public class OszkConnectorTests
     {
         [Fact]
-        public async void MekCatalog() 
+        public async void MekCatalog()
         {
             var client = new Client();
             var result = await client.FindAudioBook("gardonyi");
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void MekMp3BookPageParse()
+        {
+            string pagecontent = File.ReadAllText(@"./fixtures/mek_page_mp3.html");
+            Assert.NotEmpty(pagecontent);
+
+            var tracks = Client.ParseMekAudioBookHtml(pagecontent);
+            Assert.NotNull(tracks);
+            Assert.NotEmpty(tracks);
         }
     }
 }
