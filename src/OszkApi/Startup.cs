c12e90193817ee24aac6c25aba9c6c430e1433ef
Akos Murati <akos@murati.hu>
2020-11-13 12:57:02 +0100

    Upgrade to dotnet cor 3.1

--- src/OszkApi/Startup.cs
+++ src/OszkApi/Startup.cs
@@ -40,10 +40,10 @@ namespace OszkApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
-            app.UseMvc();
+            //app.UseMvc();
         }
     }
 }
