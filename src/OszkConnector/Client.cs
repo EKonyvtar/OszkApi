e35b744219b311a36aff7b88ec508ecd9972864a
Murati Akos <akos@murati.hu>
2024-06-02 00:09:31 +0200

    Remove headers

--- src/OszkConnector/Client.cs
+++ src/OszkConnector/Client.cs
@@ -20,9 +20,8 @@ namespace OszkConnector
             using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
             using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
             {
-                //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0(Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0");
-                client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
-                cookieContainer.Add(baseAddress, new System.Net.Cookie("SID", "5da0ab8750"));
+                //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+                //cookieContainer.Add(baseAddress, new System.Net.Cookie("SID", ""));
                 
                 // Make sure to use `GetAwaiter().GetResult()` to avoid deadlocks.
                 HttpResponseMessage response = client.GetAsync(uri).GetAwaiter().GetResult();
